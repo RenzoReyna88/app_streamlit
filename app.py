95eaae17df577ea1d34583384906a5fb3c9f7b13
Renzo Reyna <reynarenzo.88@gmail.com>
2025-01-03 22:04:05 -0300

    Mejorar tipografía de textos

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
 from PIL import Image
 
 logo = Image.open('graphmagnifier_117961.png')
 
 st.set_page_config(page_title="Calculadora de Cash Flow", page_icon=logo, layout='wide')
 
+
+
 def pagina_inicio():
 
     st.title('Cálculo de Cash Flow Automático')
-
     # Subir archivo Excel
     archivo_subido = st.file_uploader("NOTA: El archivo Excel debe contener al menos tres columnas con los nombres (fecha, ingresos y egresos) para realizar la operación.", type=["xlsx"])
 
     if archivo_subido is not None:
         # Leer el archivo Excel
         df = pd.read_excel(archivo_subido)
-
         # Manejar nombres de columnas para mayor robustez
         columnas_originales = df.columns
         columnas_normalizadas = [col.lower().strip() for col in columnas_originales]
@@ -32,7 +31,6 @@ def pagina_inicio():
             # Mostrar datos cargados
             st.write("Datos cargados de tu archivo Excel:")
             st.dataframe(df)
-
             try:
                 # Convertir la columna de fecha a datetime
                 df['fecha'] = pd.to_datetime(df['fecha'])
@@ -59,7 +57,6 @@ def pagina_inicio():
                 st.plotly_chart(fig)
 
                 st.write("Listo!! Ya tienes tu Cash Flow Calculado. Ahora puedes hacer lo que desees. Puedes interactuar en esta pantalla y tambien descargar la información si es necesario.")
-
             except Exception as e:
                 st.error(f"Error al procesar los datos: {e}")
     else:
@@ -67,15 +64,11 @@ def pagina_inicio():
 
     st.divider()
 
-    st.text(' © Creado por Renzo Reyna (Desarrollador Sarmientino) - Analista de Datos / Desarrollador Python')
-
-    st.text('Contacto:')
-    st.text('Teléfono: + 54 3525 - 620842')
-    st.text('Correo electrónico: desarrollador.sarmientino@gmail.com')
-    st.text('Domicilio: Localidad de Sarmiento, Departamento Totoral, Provincia de Córdoba, Argentina')
-
-
-
+    st.write('© Creado por Renzo Reyna (Desarrollador Sarmientino) - Analista de Datos / Desarrollador Python')
+    st.write('CONTACTO:')
+    st.write('Teléfono: + 54 3525 - 620842')
+    st.write('Correo electrónico: desarrollador.sarmientino@gmail.com')
+    st.write('Domicilio: Localidad de Sarmiento, Departamento Totoral, Provincia de Córdoba, Argentina')
 
 if __name__ == '__main__':
     pagina_inicio()
